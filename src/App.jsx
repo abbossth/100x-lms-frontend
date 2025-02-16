644ec926995c183ff141b8d062290527d401d33b
abbossth <bosskodevelopment@gmail.com>
2025-02-16 17:01:13 +0500

    tozalandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import { Routes, Route } from "react-router";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "./assets/css/templates/core.css";
-import "./assets/css/templates/theme-default.css";
-import "./assets/css/templates/demo.css";
-import "./assets/css/templates/pages/page-auth.css";
-import "./assets/css/templates/pages/page-misc.css";
-import "./assets/css/templates/pages/page-icons.css";
-import "./assets/css/templates/pages/page-account-settings.css";
+import "boxicons";
 import "./assets/css/App.css";
-
+import "bootstrap/dist/js/bootstrap.js";
 import LoginPage from "./pages/loginPage";
 import NotFound from "./pages/notFound";
 import Layout from "./layouts/layout";
@@ -18,7 +11,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<h1>Sayt qurilmoqda...</h1>}></Route>
+        <Route path="/yes" element={<Layout />}>
           <Route index element={<h1>Dashboard</h1>}></Route>
         </Route>
         <Route path="/login" element={<LoginPage />}></Route>
--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -4,41 +4,11 @@ import logo from "../assets/images/logo.png";
 const Aside = () => {
   return (
     <>
-      <aside
-        id="layout-menu"
-        class="layout-menu menu-vertical menu bg-menu-theme"
-      >
-        <div class="app-brand demo">
-          <a href="index.html" class="app-brand-link">
-            <span class="app-brand-logo demo">
-              <img src={logo} alt="logo" width={80} />
-            </span>
-            <span class="app-brand-text demo menu-text fw-bold ms-2">LMS</span>
-          </a>
-
-          <a
-            href="/"
-            class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none"
-          >
-            <i class="bx bx-chevron-left bx-sm d-flex align-items-center justify-content-center"></i>
-          </a>
+      <aside>
+        <div className="logo">
+          <img src={logo} alt="logo" />
         </div>
-
-        <div class="menu-inner-shadow"></div>
-
-        <ul class="menu-inner py-1">
-          <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">Apps &amp; Pages</span>
-          </li>
-          <li class="menu-item">
-            <a href="/users" target="_blank" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-envelope"></i>
-              <div class="text-truncate" data-i18n="Users">
-                Users
-              </div>
-            </a>
-          </li>
-        </ul>
+        <h1>Logo</h1>
       </aside>
     </>
   );
